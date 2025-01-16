d1d03aac56dc06cbaf6844490255d952702aaa35
leonleoff <76099309+leonleoff@users.noreply.github.com>
2025-01-16 19:31:07 +0100

    fixed bug with tranforming inputs

--- stable_plugins/state_analysis/lineardependence/tasks.py
+++ stable_plugins/state_analysis/lineardependence/tasks.py
@@ -19,33 +19,51 @@ TASK_LOGGER = get_task_logger(__name__)
     bind=True,
 )
 def lineardependence_task(self, db_id: int) -> str:
+
     TASK_LOGGER.info(f"Starting lineardependence task with db id '{db_id}'")
     task_data = ProcessingTask.get_by_id(id_=db_id)
     if not task_data:
         msg = f"Could not load task data with id {db_id}!"
         TASK_LOGGER.error(msg)
         raise KeyError(msg)
+
     # Get and Log parameters
     parameters = loads(task_data.parameters or "{}")
     TASK_LOGGER.info(f"Parameters: {parameters}")
 
+    # Get and log values
     vectors = parameters.get("vectors", [])
-    tolerance = parameters.get("tolerance", 1e-10)
+    tolerance = parameters.get("tolerance")
+    TASK_LOGGER.info(
+        f"Parameters before transforming: vectors={vectors}, tolerance={tolerance}"
+    )
 
-    TASK_LOGGER.info(f"Parameters: vectors={vectors}, tolerance={tolerance}")
+    # TODO: check input hier nochmal??? herausfinden dann mit der api
+
+    # Transform vectors
+    newSetofVectors = []
+    try:
+        for vector in vectors:
+            complexNumbers = []
+            for tupel in vector:
+                complexNumber = complex(tupel[0], tupel[1])
+                complexNumbers.append(complexNumber)
+            npVector = np.array(complexNumbers)
+            newSetofVectors.append(npVector)
+    except Exception as e:
+        TASK_LOGGER.error(f"Error in transforming vectors: {e}")
+        raise
 
     try:
-        # Convert vectors to numpy arrays
-        numpy_vectors = [np.array(vector, dtype=complex) for vector in vectors]
-
         # Log the call to the function for checking linear dependence
         TASK_LOGGER.info(
             "Calling the function to check linear dependence with parameters: "
-            f"vectors={numpy_vectors}, tolerance={tolerance}"
+            f"vectors={newSetofVectors}, tolerance={tolerance}"
         )
+
         # Call the function to check linear dependence
         result = are_vectors_linearly_dependent(
-            vectors=numpy_vectors, tolerance=tolerance
+            vectors=newSetofVectors, tolerance=tolerance
         )
 
         # Save the result as a file
